a8a8005fb72f805dfb9aec47eb10922ba3d61c9a
Hamzach2 <chami.hamza.25@gmail.com>
2023-08-13 20:02:20 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./template/**/*.{html,js}"],
+  theme: {
+    height: {
+      lil: '2px',
+      sm: '8px',
+      md: '16px',
+      lg: '24px',
+      xl: '48px',
+      bar: '500px',
+      nar: '250px',
+      far:'1440px'
+     },
+    extend: {},
+  },
+  plugins: [],
+}
+
