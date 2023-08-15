0b38b6edab7d5c65d95e770f75bcbb66d04ce97a
Hamzach2 <chami.hamza.25@gmail.com>
2023-08-15 15:38:44 +0100

    7q

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: ["./template/**/*.{html,js}"],
   theme: {
+    extend: {
     height: {
       lil: '2px',
       sm: '8px',
@@ -12,7 +13,15 @@ module.exports = {
       nar: '250px',
       far:'1440px'
      },
-    extend: {},
+     dropShadow: {
+      '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+      '4xl': [
+          '0 35px 35px rgba(0, 0, 0, 0.25)',
+          '0 45px 65px rgba(0, 0, 0, 0.15)'
+      ]
+    }
+    },
+    
   },
   plugins: [],
 }
